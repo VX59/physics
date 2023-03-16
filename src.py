b3ff2e996aaac2db0d66caa186fe3375a0011578
rsenic <derosajb19@gmail.com>
2023-03-16 17:46:47 -0400

    adding equations

--- src.py
+++ src.py
@@ -1,31 +1,35 @@
 import numpy as np
+import math
 import os
 
 class Environment(object):
-    g = 9.8         # m/s
+    g = 9.81        # m/s
     G = 6.67e-11    # gravitational constant
-    def __init__(self,**kwargs):
-        self.yi = kwargs['yi']        # y initial
    
 class Normal_Object(Environment) :
     def __init__(self,**kwargs):
-        self.mass = kwargs['mass']      # mass
-        self.vi = kwargs['vi']          # velocity initial
-        self.theta = kwargs['theta']    # angle of action
+        for key, val in kwargs.items():
+            if(key == 'yi'): self.yi = kwargs['yi']          # y initial
+            if(key == 'yf'): self.yf = kwargs['yf']          # y final
+            if(key == 'mass'): self.mass = kwargs['mass']      # mass
+            if(key == 'vi'): self.vi = kwargs['vi']          # velocity initial
+            if(key == 'theta'): self.theta = kwargs['theta']    # angle of action
 
-class Calculate(object):
-    def G_Work_Instant(self,object:Normal_Object, env:Environment):
-        W = object.mass * env.g * env.yi # J
+class Calculate(Environment):
+    def G_Work_Instant(self,obj:Normal_Object):
+        W = obj.mass * super().g * obj.yi # J
         print(W)
         return W
 
-    def Change_U(self,W:float): 
+    def Change_U(self, W:float): 
         print(-W)
         return -W #J
-
-env = Environment(yi=8.38)
-snowball = Normal_Object(mass=2.5,vi=10.8,theta=40)
+    
+    def Get_Vf(self, obj:Normal_Object):
+        vf = math.sqrt(math.pow(obj.vi, 2) + (super().g * obj.yi)- (super().g*obj.yf))
+        print(vf)
+        return vf
+rollerCoaster = Normal_Object(mass=831,vi=16.0 , yi=43.0, yf=0)
 
 calculator = Calculate()
-W = calculator.G_Work_Instant(snowball, env)
-calculator.Change_U(W)
+V = calculator.Get_Vf(rollerCoaster)
